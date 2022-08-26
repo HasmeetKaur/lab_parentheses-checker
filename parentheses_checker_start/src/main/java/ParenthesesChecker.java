659a7185d42d9e5c5aaf44b05d191d7057751b58
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-08-26 10:26:24 +0100

    added angle bracket

--- parentheses_checker_start/src/main/java/ParenthesesChecker.java
+++ parentheses_checker_start/src/main/java/ParenthesesChecker.java
@@ -1,11 +1,16 @@
 import java.util.Stack;
 
 public class ParenthesesChecker {
+    // Ensure that number of opening brackets match closing brackets
+    // Check total bracket count is even/zero
+    // Ensure that the different brackets have a corresponding closed bracket
+    // Ensure that the order is correct
+
         public static boolean checkParentheses(String testString) {
             Stack<Character> stack  = new Stack<Character>();
             for(int i = 0; i < testString.length(); i++) {
                 char c = testString.charAt(i);
-                if(c == '[' || c == '(' || c == '{' ) {
+                if(c == '[' || c == '(' || c == '{' || c == '<' ) {
                     stack.push(c);
                 } else if(c == ']') {
                     if(stack.isEmpty() || stack.pop() != '[') {
@@ -17,6 +22,10 @@ public class ParenthesesChecker {
                     }
                 } else if(c == '}') {
                     if(stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                } else if(c == '>') {
+                    if(stack.isEmpty() || stack.pop() != '<') {
                         return false;
                     }
                 }
